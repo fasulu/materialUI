0a8da9781ad012653ed46af95579c0148af620f2
rfasulu <rfasulu@hotmail.com>
2021-07-28 12:39:31 +0200

    Drawer added

--- materialui/src/components/Layout.js
+++ materialui/src/components/Layout.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';     // import makeStyles
+import Drawer from '@material-ui/core/Drawer';      // import Drawer
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240
 
 const useStyles = makeStyles({                      // create and declare styles
     pages: {
         background: '#d5d5d5',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    root: {
+        display:'flex'
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        background:'#f9f9f9'
     }
 })
 
@@ -14,7 +28,20 @@ export default function Layout({ children }) {      // pass the children under L
 
     return (
 
-        <div>
+        <div className={styles.root}>
+
+            <Drawer                                 //* create drawer to show permanently in left and over-ride materialUI paper classes by our own drawerPaper styles
+                className={styles.drawer}
+                variant='permanent'
+                anchor='left'
+                classes={{paper: styles.drawerPaper}}>  
+
+                <div>
+                    <Typography variant='h5' >
+                        Address
+                    </Typography>
+                </div>
+            </Drawer>
 
             <div className={styles.pages}>          {/* use those styles in return*/}
 
